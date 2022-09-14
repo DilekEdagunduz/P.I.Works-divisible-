46f9529362a5b7c4f071420433d5bc1dfec82e43
asus <asus@DESKTOP-7AA3IH2>
2022-09-14 17:49:47 +0300

    technical question

---
+++ Divisible/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Divisible
+{
+    class Program
+    {
+        
+        // by sum of its digits
+        static String is_divisible(long input)
+        {
+            long temp = input;
+
+            // sum of digits
+            int sum = 0;
+            while (input != 0)
+            {
+                int k = (int)input % 10;
+                sum += k;
+                input /= 10;
+            }
+
+            // check if sum of digits divides 
+            if (temp % sum == 0)
+                return "YES";
+
+            return "NO";
+        }
+
+        public static void Main()
+        {
+            long input = 75;
+            Console.WriteLine(is_divisible(input));
+        }
+    }
+}
